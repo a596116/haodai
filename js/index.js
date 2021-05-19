2f794b3c5e88e5e7d06dba16ba46377cb55fe4be
haodai <57907386+a596116@users.noreply.github.com>
2021-05-19 18:24:04 +0800

    web19
    web19

--- js/index.js
+++ js/index.js
@@ -90,3 +90,48 @@ function openMenu(evt, menuName) {
 //   setTimeout(showSlides, 2000); // Change image every 2 seconds
 // }
 /*------------------------------------------------------------------------------------------- */
+
+
+
+$(function(){
+	$(window).load(function(){
+		$(window).bind('scroll resize', function(){
+			var $this = $(this);
+			var $this_Top=$this.scrollTop();
+			
+			//當高度小於100時，關閉區塊	
+			// if($this_Top < 100){
+			// 	$('#top-bar').stop().animate({top:"-65px"});
+			// 	}
+      var h1 = document.getElementById("home").scrollHeight;
+      var h2 = document.getElementById("home").scrollHeight + document.getElementById("box").scrollHeight;
+      var h3 = document.getElementById("home").scrollHeight + document.getElementById("box").scrollHeight + document.getElementById("Data").scrollHeight;
+      var h4 = document.getElementById("home").scrollHeight + document.getElementById("box").scrollHeight + document.getElementById("Data").scrollHeight + document.getElementById("horse").scrollHeight;
+      // var h5 = document.getElementById("home").scrollHeight + document.getElementById("box").scrollHeight + document.getElementById("Data").scrollHeight + document.getElementById("horse").scrollHeight + document.getElementById("ma").scrollHeight;
+
+			if($this_Top < h1-190){
+        document.querySelector('.bar-item.home').style = "background-color:black";}
+      else{document.querySelector('.bar-item.home').style = "background-color:none";}
+
+			if($this_Top > h1-190 && $this_Top < h2-500){
+        document.querySelector('.bar-item.box').style = "background-color:black";}
+        else{document.querySelector('.bar-item.box').style = "background-color:none";}
+
+      if($this_Top > h2-500 && $this_Top < h3-550){
+        document.querySelector('.bar-item.data').style = "background-color:black";}
+        else{document.querySelector('.bar-item.data').style = "background-color:none";}
+
+      if($this_Top > h3-550 && $this_Top < h4-650){
+        document.querySelector('.bar-item.horse').style = "background-color:black";}
+        else{document.querySelector('.bar-item.horse').style = "background-color:none";}
+
+      if($this_Top > h4-650){
+        document.querySelector('.bar-item.ma').style = "background-color:black";}
+        else{document.querySelector('.bar-item.ma').style = "background-color:none";}
+
+
+
+
+		}).scroll();
+	});
+});
